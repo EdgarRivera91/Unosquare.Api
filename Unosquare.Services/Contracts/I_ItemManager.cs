a72516a75568edc131caaa3c9629ce452698f714
Eduardo Torres <z3r0maker@gmail.com>
2021-08-24 09:06:55 -0500

    Added TODOs

--- Unosquare.Services/Contracts/I_ItemManager.cs
+++ Unosquare.Services/Contracts/I_ItemManager.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace Unosquare.Services.Contracts
 {
+    //TODO: Name of the interfaces should not have underscored, this should be named as IItemManager
+    //TODO: The generic repository pattern should be applied at an entity level not at a service level? 
     public interface I_ItemManager<TEntity>
     {
         IEnumerable<TEntity> GetAll();
-        TEntity Get(long id);
+        TEntity Get(long id); 
+        // TODO: This can contain a parameter of IEnumerable<TEntity> Get(
+        //Expression<Func<TEntity, bool>> filter = null,
+        //Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, 
         void Add(TEntity entity);
         void Update(TEntity dbEntity, TEntity entity);
         void Delete(TEntity entity);
